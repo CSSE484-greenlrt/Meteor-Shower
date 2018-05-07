b61cda72b9c96c31492729c23331703fb9aeb002
Ryan Greenlee <ryan.greenlee1@gmail.com>
2018-05-07 11:29:58 -0400

    Initial Commit

---
+++ Meteor Shower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meteor Shower
+//
+//  Created by Ryan Greenlee on 5/7/18.
+//  Copyright © 2018 Ryan Greenlee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
